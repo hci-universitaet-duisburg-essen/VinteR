6ff5e95db28fb95db349e76211928d5cd484091e
Nils Verheyen <nverheyen@googlemail.com>
2018-05-22 15:23:13 +0200

    Added loading of configuration and retrieval of service via di

---
+++ VinteR/Configuration/VinterConfigurationService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Schema;
+
+namespace VinteR.Configuration
+{
+    public class VinterConfigurationService : IConfigurationService
+    {
+        private JSchema _schema;
+        private Configuration _configuration;
+
+        public Configuration GetConfiguration()
+        {
+            if (_configuration == null)
+            {
+                LoadConfiguration();
+            }
+
+            return _configuration;
+        }
+
+        private void LoadConfiguration()
+        {
+            LoadSchema();
+            var serializer = new JsonSerializer();
+            var json = ReadJson("vinter.config.json");
+            using (var jsonReader = new JsonTextReader(new StringReader(json)))
+            {
+                var validatingReader = new JSchemaValidatingReader(jsonReader) { Schema = _schema };
+
+                this._configuration = JsonConvert.DeserializeObject<Configuration>(json);
+            }
+        }
+
+        private void LoadSchema()
+        {
+            var serializer = new JsonSerializer();
+            var json = ReadJson("vinter.config.schema.json");
+
+            this._schema = JSchema.Parse(json);
+        }
+
+        private static string ReadJson(string file)
+        {
+            using (var reader = new StreamReader(file))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
