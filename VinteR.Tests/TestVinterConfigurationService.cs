e4d39556ce99f477ec9b576f9110f2f40e1e1896
Nils Verheyen <nverheyen@googlemail.com>
2018-05-23 15:01:50 +0200

    Added test project

---
+++ VinteR.Tests/TestVinterConfigurationService.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using VinteR.Configuration;
+
+namespace VinteR.Tests
+{
+    [TestFixture]
+    public class TestVinterConfigurationService
+    {
+        [Test]
+        public void TestGetConfiguration()
+        {
+            var configurationService = new VinterConfigurationService();
+            var configuration = configurationService.GetConfiguration();
+            Assert.IsNotNull(configuration);
+        }
+    }
+}
