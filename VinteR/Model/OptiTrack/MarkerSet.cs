877a1d7c84832c78769fbea5eefedc7c8739c2a2
Cedric <cedric.krause@stud.uni-due.de>
2018-06-04 10:45:06 +0200

    Markersets from FrameOfMocapData into MocapFram

---
+++ VinteR/Model/OptiTrack/MarkerSet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace VinteR.Model.OptiTrack
+{
+    class MarkerSet : OptiTrackBody
+    {
+        /// <summary>
+        /// List of Markers in the markerset
+        /// </summary>
+        public IList<Point> Markers
+        {
+            get => _markers;
+            set
+            {
+                if (value == null) _markers.Clear();
+                else _markers = value;
+            }
+        }
+
+        private IList<Point> _markers;
+
+        public MarkerSet(string id) : base(id)
+        {
+            this._markers = new List<Point>();
+            this.Type = EBodyType.MarkerSet;
+        }
+    }
+}
