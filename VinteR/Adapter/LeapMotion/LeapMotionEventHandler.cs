bd3d52105c4a0bf7aa9e447ca5ced1c1afca8997
Sarah Theußen <sarah.theussen@stud.uni-due.de>
2018-06-20 11:25:41 +0200

    Fixed logging for different log levels of leap motion adapter and merger
    Change-Id: Iac03bca53a46f63825b6006976b2990005c8b012

--- VinteR/Adapter/LeapMotion/LeapMotionEventHandler.cs
+++ VinteR/Adapter/LeapMotion/LeapMotionEventHandler.cs
@@ -42,20 +42,19 @@ namespace VinteR.Adapter.LeapMotion
         {
             // Get the most recent frame and report some basic information
             Frame frame = args.frame;
-
-            //Logger.Info("Leap Motion Frame Available");
-            //Logger.Info("Frame id: {0}, hands: {1}", frame.Id, frame.Hands.Count);
+            
+            Logger.Debug("Frame available - frame id: {0}, hands: {1}", frame.Id, frame.Hands.Count);
 
             // Create Mocap Frame to send to server later
             VinteR.Model.MocapFrame mocapFrame = new VinteR.Model.MocapFrame(adapter.Config.Name, adapter.Config.AdapterType);
 
             foreach (Leap.Hand hand in frame.Hands)
             {
-                //Logger.Info("Leap Hand id: {0}, fingers: {1}", hand.Id, hand.Fingers.Count);
+                Logger.Debug("Hand id: {0}, fingers: {1}", hand.Id, hand.Fingers.Count);
 
                 // Get the Arm bone
                 // Arm arm = hand.Arm;
-                // Logger.Info("Arm direction: {0}, wrist position: {1}, elbow position: {2}", arm.Direction, arm.WristPosition, arm.ElbowPosition);
+                // Logger.Debug("Arm direction: {0}, wrist position: {1}, elbow position: {2}", arm.Direction, arm.WristPosition, arm.ElbowPosition);
 
                 // Get fingers and bones
                 IList<Model.LeapMotion.Finger> fingers = new List<Model.LeapMotion.Finger>();
@@ -65,8 +64,7 @@ namespace VinteR.Adapter.LeapMotion
                     Model.LeapMotion.Finger modelFinger = new Model.LeapMotion.Finger(getFingerType(finger.Type.ToString()));
                     IList<Model.LeapMotion.FingerBone> fingerBones = new List<Model.LeapMotion.FingerBone>();
 
-                    //Logger.Info("Leap Finger {0}: length: {1}mm, width: {2}mm, fingertipPosition: {3}", finger.Type.ToString(), finger.Length, finger.Width, finger.TipPosition);
-                    //Logger.Info("Model Finger {0}", getFingerType(finger.Type.ToString()));
+                    Logger.Debug("Finger {0}: length: {1}mm, width: {2}mm, fingertipPosition: {3}", finger.Type.ToString(), finger.Length, finger.Width, finger.TipPosition);
 
                     // Get finger bones
                     Bone bone;
@@ -80,8 +78,7 @@ namespace VinteR.Adapter.LeapMotion
 
                         fingerBones.Add(modelBone);
 
-                        //Logger.Info("Leap Bone: {0}, start: {1}, end: {2}", bone.Type.ToString(), bone.PrevJoint, bone.NextJoint);
-                        //Logger.Info("Model Bone: {0}, start: {1}, end: {2}", getFingerBoneType(bone.Type.ToString()), modelBone.LocalStartPosition, modelBone.LocalEndPosition);
+                        Logger.Debug("Finger Bone: {0}, start: {1}, end: {2}", bone.Type.ToString(), bone.PrevJoint, bone.NextJoint);
                     }
 
                     modelFinger.Bones = fingerBones;
@@ -95,7 +92,6 @@ namespace VinteR.Adapter.LeapMotion
                 float pitch = direction.Pitch * 180.0f / (float)Math.PI;
                 float roll = normal.Roll * 180.0f / (float)Math.PI;
                 float yaw = direction.Yaw * 180.0f / (float)Math.PI;
-                //Logger.Info("Leap Hand pitch: {0} degrees, roll: {1} degrees, yaw: {2} degrees", pitch, roll, yaw);
 
                 // Create Model hand
                 Model.LeapMotion.Hand modelHand = new Model.LeapMotion.Hand();
@@ -104,10 +100,8 @@ namespace VinteR.Adapter.LeapMotion
                 modelHand.Side = getHandSideType(hand);
                 modelHand.Fingers = fingers;
                 modelHand.Name = hand.IsLeft ? NameHandLeft : NameHandRight;
-                //Logger.Info("Model Hand side: {0}", modelHand.Side.ToString());
 
                 // Create hand body to add to mocapFrame
-                Logger.Info("Hand Model: " + modelHand.Fingers.Count);
                 VinteR.Model.Body modelBody = modelHand;
                 mocapFrame.AddBody(ref modelBody);
             }
